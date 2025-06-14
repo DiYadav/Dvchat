2720f713ee7d1890d681726b8f66d975f1344d82
Dinesh <yadavdinesh2852002@gmail.com>
2025-06-14 17:32:51 +0530

    Delete Unnecessary file

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',       // <– You said your HTMLs are here
+    './static/src/**/*.js',        // optional if JS uses Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
