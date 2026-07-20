e08962b68e7fba3c5b0915fddcc8eec9b1803f4e
Dinesh Yadav <yadavdinesh2852002@gmail.com>
2026-07-20 10:40:47 +0530

    added login/register temp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
+import Register from "./pages/Register";  
 
-// Temporary pages (replace later)
-function Register() {
-    return <h1>Register Page</h1>;
-}
-
+// Temporary Home page
 function Home() {
     return <h1>Home Page</h1>;
 }
@@ -16,31 +13,26 @@ function App() {
         <BrowserRouter>
             <Routes>
 
-                {/* Redirect root to login */}
                 <Route
                     path="/"
                     element={<Navigate to="/login" replace />}
                 />
 
-                {/* Login */}
                 <Route
                     path="/login"
                     element={<Login />}
                 />
 
-                {/* Register */}
                 <Route
                     path="/register"
                     element={<Register />}
                 />
 
-                {/* Home */}
                 <Route
                     path="/home"
                     element={<Home />}
                 />
 
-                {/* 404 */}
                 <Route
                     path="*"
                     element={<h1>404 - Page Not Found</h1>}
